3ef80181c4f680658ad31ade452f97ab5439a66f
benjamin <benjamin1993112@gmail.com>
2025-05-26 08:40:03 +0700

    deploy with domain 2

--- first/src/config.js
+++ first/src/config.js
@@ -2,7 +2,7 @@ const STAGE_API = process.env.REACT_APP_API_URL;
 
 const config = {
     development: {
-        API_URL: 'http://172.105.92.67:8000/',
+        API_URL: 'http://172.105.92.67:8000',
     },
     production: {
         API_URL: STAGE_API ? STAGE_API: 'http://16.171.15.89:8000'
