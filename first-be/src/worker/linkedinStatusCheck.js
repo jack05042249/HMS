0a8bb9082e64e9d8d575e7737f9281cc00d8fca1
benjamin <benjamin1993112@gmail.com>
2025-06-28 01:37:38 +0800

    linkedinStatusCheck with scrapingdog

--- first-be/src/worker/linkedinStatusCheck.js
+++ first-be/src/worker/linkedinStatusCheck.js
@@ -1,71 +1,23 @@
-const { last } = require('lodash')
 const { Talent, Organization } = require('../models')
 const moment = require('moment')
 const { chromium } = require('playwright')
 
+const axios = require('axios')
+const API_KEY = '685e3b2a640d9eec39199c4a'
+const apiUrl = 'https://api.scrapingdog.com/linkedIn'
+
+function getLinkIdFromLinkedInUrl(url) {
+  // This regex matches the part after '/in/' and before the next '/' or end of string
+  const match = url.match(/linkedin\.com\/in\/([^\/]+)/i);
+  return match ? match[1] : null;
+}
+
 const linkedinStatusCheck = async () => {
-  // Perform LinkedIn profile check
-  const browser = await chromium.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled']
-  })
-  const browsercontext = await browser.newContext({
-    userAgent:
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
-      '(KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    viewport: { width: 1280, height: 800 },
-    locale: 'en-US',
-    deviceScaleFactor: 1,
-    isMobile: false,
-    hasTouch: false
-  })
-   // Set the li_at cookie
-  await browsercontext.addCookies([{
-    name: 'li_at',
-    value: 'AQEDAVwV3GECmeHgAAABl6EcghkAAAGXxSkGGU4ARgzP8iPB5jKumtn-nRWWZLWV1ZmpCh4LwCNHDyequXo2dgkM7KXB-y5Q2OslZ5YEUuFXVM3not9BMGpUqgBk8KD_Xy8pImarufSW7qG6soVjY5e3',
-    domain: '.linkedin.com',
-    path: '/',
-    httpOnly: true,
-    secure: true,
-    sameSite: 'Lax'
-  }]);
-  let page = await browsercontext.newPage()
-
-  // Optional stealth tricks
-  await page.addInitScript(() => {
-    Object.defineProperty(navigator, 'webdriver', { get: () => false })
-    window.chrome = {
-      runtime: {}
-      // any more required props...
-    }
-    Object.defineProperty(navigator, 'languages', {
-      get: () => ['en-US', 'en']
-    })
-    Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] })
-  })
-  // await page.goto('https://www.linkedin.com', { waitUntil: 'domcontentloaded', timeout: 200000 })
-//   try {
-//     await page.evaluate(() => {
-//       [...document.querySelectorAll('button, a')].find(el => el.innerText.trim().toLowerCase() === 'sign in').click();
-//     })
-//     await page.waitForTimeout(5000) // Wait for the modal to open
-//     await page.evaluate(() => {
-//       document.querySelector('input[name="session_key"]').value = 'nguyen.vc.2201@gmail.com';
-//       document.querySelector('input[name="session_password"]').value = 'Hi!Nguyen?';
-//       [...document.querySelectorAll('button, a')].find(el => el.innerText.trim().toLowerCase() === 'sign in').click()
-//     })
-//     await page.waitForTimeout(20000) // Wait for the sign-in to complete
-//     await page.waitForLoadState('domcontentloaded')
-
-//     console.log('✅ Successfully clicked the sign-in modal button.')
-//   } catch (error) {
-//     console.log('⚠️ Failed to click the sign-in modal button:', error.message)
-//   }
   const talents = await Talent.findAll()
   let organizations = await Organization.findAll({
     attributes: ['name']
   })
-  organizations.push('Commit Offshore');
+  organizations.push('Commit Offshore')
   console.log(`Found ${talents.length} talents and ${organizations.length} organizations.`)
   for (const talent of talents) {
     // Check LinkedIn status for each talent
@@ -75,46 +27,71 @@ const linkedinStatusCheck = async () => {
       await talent.save()
       continue
     }
-    const url = talent.linkedinProfile;
+    const url = talent.linkedinProfile
     if (url) {
-        // if (talent.email == 'tatyana.matlasch@gmail.com' || talent.email == 'mila@itsoft.co.il' || talent.email == 'sona@itsoft.co.il' || talent.email == 'derkonstantin@gmail.com') {
-          await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 200000 })
-          await page.waitForTimeout(5000) // Wait for the page to load
-          let presentCompany = await page.evaluate(() => {
-            const companyElements = document.querySelectorAll('li.UKjiyRIZvOvdFsmmUeAJMHKloPTPxiDYQrjI');
-            let presentCompany = [];
+      if (talent.email == 'samvelgharibyan1996@gmail.com') {
+        console.log(url, apiUrl)
+        let linkId = getLinkIdFromLinkedInUrl(url)
+        if (!linkId) {
+          console.log('No linkId found in URL:', url)
+          continue
+        }
 
-            companyElements && companyElements.forEach(element => {
-              const periods = Array.from(element.querySelectorAll('span.pvs-entity__caption-wrapper'));
-              if (periods && periods.some(period => period.textContent.trim().toLowerCase().includes('present'))) {
-                let name = '';
-                if (periods.length > 1) {
-                  name = element.querySelector('div > div.display-flex.flex-column.align-self-center.flex-grow-1 > div.display-flex.flex-row.justify-space-between > a > div > div > div > div > span:nth-child(1)');
-                } else {
-                  name = element.querySelector('div > div.display-flex.flex-column.align-self-center.flex-grow-1 > div.display-flex.flex-row.justify-space-between > a > span:nth-child(2) > span:nth-child(1)');
+        console.log(linkId);
+        let presentNum = 0, lastCompany = '';
+
+        const params = {
+          api_key: API_KEY,
+          type: 'profile',
+          linkId: linkId,
+          private: 'false'
+        }
+
+        await axios
+          .get(apiUrl, { params: params })
+          .then(function (response) {
+            if (response.status === 200) {
+              const data = response.data;
+              const experience = data[0].experience || [];
+
+              console.log(experience);
+
+              experience.map(exp => {
+                if (exp.ends_at === null) {
+                  presentNum = -1;
+                  lastCompany = exp.company_name ? exp.company_name.replace(/[^a-zA-Z0-9\s]/g, '').trim() : '';
+                } else if (exp.ends_at === 'Present') {
+                  presentNum++;
+                  lastCompany = exp.company_name ? exp.company_name.replace(/[^a-zA-Z0-9\s]/g, '').trim() : '';
                 }
-                if (name) presentCompany.push(name.textContent.trim().replace(/\s+/g, " "))
+              });
+              console.log('Present Number:', presentNum, 'Last Company:', lastCompany);
+
+              if (presentNum == 1 || presentNum == -1) {
+                if (organizations.some(org => lastCompany.toLowerCase().includes(org.name.toLowerCase()))) {
+                  talent.linkedinProfileChecked = true;
+                } else {
+                  talent.linkedinProfileChecked = false;
+                }
+              } else {
+                talent.linkedinProfileChecked = false;
               }
-            });
-            console.log(companyElements && companyElements.length, ' -- ', presentCompany.length);
-            return presentCompany;
-          })
-          console.log(presentCompany.length, ' -- ', presentCompany);
-          let lastCompany = '';
-          if (presentCompany.length == 1) {
-            if (organizations.some(org => presentCompany[0].toLowerCase().includes(org.name.toLowerCase()))) {
-              lastCompany = presentCompany[0];
+              console.log('LinkedIn profile checked:', talent.linkedinProfileChecked);
+            } else {
+              talent.linkedinProfileChecked = false;
+              console.log('Request failed with status code: ' + response)
             }
-          }
-          console.log(`Last company for ${talent.fullName} is: ${lastCompany}`);
-          if (lastCompany) {
-            talent.linkedinProfileChecked = true
-          } else {
-            talent.linkedinProfileChecked = false
-          }
-        // } else continue
+          })
+          .catch(function (error) {
+            talent.linkedinProfileChecked = false;
+            console.error('Error making the request: ' + error.message)
+          })
+      } else continue
     } else {
       talent.linkedinProfileChecked = false
+    }
+    if (talent.email == 'samvelgharibyan1996@gmail.com') {
+      console.log(`Talent ${talent.name} LinkedIn profile checked: ${talent.linkedinProfileChecked}`);
     }
     talent.linkedinProfileDate = moment().format()
     await talent.save()
